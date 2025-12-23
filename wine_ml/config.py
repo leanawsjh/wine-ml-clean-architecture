59edc4a455e69461978569d5cc38594e3358d129
javedhassans <javedhassans@outlook.com>
2025-12-23 06:13:47 +0530

    config file

---
+++ wine_ml/config.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+@dataclass
+class TrainConfig:
+    test_size: float = 0.2
+    randon_state: int = 42
+    n_estimators: int = 100
+    max_depth: int | None = None
